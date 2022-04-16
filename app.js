f853210fca9698dfa3fcc3c7f9b233da19c6113b
Shubham Patel <sp.nobody12@gmail.com>
2022-04-16 11:21:59 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const homeStartingContent = "Use '+' in right corner for creating new post.";
+const aboutContent = "Create your own post using + in the right corner with 'Title' and 'Content' and Save here forever and Access whenever you want.";
+const contactContent = "Contact Me :";
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res) {
+  Post.find({}, function(err, foundPost) {
+    res.render("home", {
+      content: homeStartingContent,
+      posts: foundPost
+    });
+  });
+
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+    content: aboutContent
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    content: contactContent
+  });
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postContent
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
+
+});
+
+app.get("/posts/:postId", function(req, res) {
+  const requestPostId = req.params.postId;
+  Post.findOne({
+    title: requestPostId
+  }, function(err, foundPost) {
+    // // javascript lodash library to remove all the capitalisation and dashes between the text
+    // // like Day1 or Day-1
+    // if (_.lowerCase(post.title) === _.lowerCase(routeParameter)) {
+      res.render("post", {
+        title: foundPost.title,
+        content: foundPost.content
+      });
+  });
+});
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
